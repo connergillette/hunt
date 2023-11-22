cf4ca5e1364aa92a2e6a0f67e225f521035af885
connergillette <connerwgillette@gmail.com>
2023-11-21 16:48:32 -0800

    Adjust label leading

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -11,7 +11,7 @@ interface Props extends PropsWithChildren {
 
 export default function Input({ type = 'text', value, name, placeholder, onChange, required }: Props) {
   return (
-    <label className="flex flex-col w-full text-xs">
+    <label className="flex flex-col w-full text-xs leading-loose">
       {placeholder}
       <input id={name} type={type} value={value} name={name} accept=".csv" className="rounded-md text-base bg-gray-300 p-2 my-auto self-center text-black w-full" onChange={onChange} required={required || false}></input>
     </label>
